6703f618710f565649724ffcacd1e200fcbc1363
rahulmitraofficial <69386022+rahulmitraofficial@users.noreply.github.com>
2020-09-30 14:07:54 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -148,7 +148,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                     [
                         [
                             InlineKeyboardButton(
-                                text="Add Pɧơɛnıх to your group.",
+                                text="Add Danger Dog to your group.",
                                 url="t.me/{}?startgroup=botstart".format(bot.username),
                             )
                         ]
